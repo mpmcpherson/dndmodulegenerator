632fc2b8ab8b2f8402a0f79e80ceacceda54b0d7
Michael McPherson <michael@mpmcpherson.com>
2020-12-25 00:09:49 -0500

    wiring up logging

--- site/pierrefrancoisdulac.php
+++ site/pierrefrancoisdulac.php
@@ -8,7 +8,7 @@ echo "<div class='logme'>".$_SERVER['HTTP_USER_AGENT']."</div>";
 $ua_string = $_SERVER['HTTP_USER_AGENT'];
 $ipAdd = $_SERVER['REMOTE_ADDR'];
 
-$qq = "INSERT INTO logging(`UA_String` , `ipAdd`, `logTime`, `programId`) VALUES ('".$ua_string."','".$ipAdd."','".gmdate('Y-m-d H:i:s')."','".strval(1)."')";//should actually rig up a call-and-response here so each program knows its id
+$qq = "INSERT INTO logging(`UA_String` , `ipAdd`, `logTime`, `programId`) VALUES ('".$ua_string."','".$ipAdd."','".gmdate('Y-m-d H:i:s')."','".strval(6)."')";//should actually rig up a call-and-response here so each program knows its id
 
 $result = mysqli_query($dbhandle, $qq);
 
